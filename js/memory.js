d86a73cdc58ea0cd1c50b52ffcf5cce20cbfdea1
Laia Markixa <laiamr77@gmail.com>
2022-03-24 10:26:09 +0100

    index.js added, updating repo to remote

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,38 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return
+    }
+    for (let i = 0; i < this.cards.length; i++) {
+      const randomCardIndex = Math.floor(Math.random() * this.cards.length);
+      const randomCard = this.cards[randomCardIndex];
+      
+      this.cards[randomCardIndex] = this.cards[i];
+      this.cards[i] = randomCard;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked ++;
+    if(card1 === card2){
+      this.pairsGuessed++;
+      return true;
+    } 
+    return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if(this.pairsGuessed === (this.cards.length / 2)){
+      return true;
+    }
+    return false;
   }
 }
 
